6a680bc3cc2d845b357ff23c2d70bdfd8be4fdf6
remkeah <gmasiewicz@gmail.com>
2024-01-29 08:00:17 +0100

    final commit

---
+++ app/src/main/java/com/example/webservice/Book.java
@@ -0,0 +1,38 @@
+package com.example.webservice;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Book {
+    @SerializedName("title")
+    private String title;
+    @SerializedName("author_name")
+    private List<String> authors;
+    @SerializedName("cover_i")
+    private String cover;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+}
